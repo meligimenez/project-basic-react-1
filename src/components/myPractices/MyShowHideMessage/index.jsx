660ef442178b0fe26585532d88083ed6614c0b87
meligimenez <melinagimenez.dev@gmail.com>
2023-01-19 13:52:02 -0300

    dynamic background: MyShowHideMessage

--- src/components/myPractices/MyShowHideMessage/index.jsx
+++ src/components/myPractices/MyShowHideMessage/index.jsx
@@ -1,13 +1,35 @@
-import { Container, Row, Col, Button, Toast } from "react-bootstrap"
+import { useRef, useState } from "react"
+import { Container, Row, Col, Button, Toast, Form } from "react-bootstrap"
 import { useShow } from "../../../hook/useShow"
 
 export const MyShowHideMessage = () => {
   const { show, handleShowMessage } = useShow(false)
+  const inputRef = useRef(null)
+  const [fondoImage, setFondoImage] = useState('none')
+
+  const changeImg = () => {
+    const valueInput = inputRef.current?.value;
+    if (valueInput) {
+      setFondoImage(valueInput)
+    } else {
+      setFondoImage('none')
+    }
+
+    console.log('FONDO IMAGE', fondoImage)
+    console.log('VALUE INPUT', valueInput)
+  }
 
   return (
-    <Container style={{ height: '90vh', background: !show ? 'url(https://s1.1zoom.me/big0/22/Penguins_Happy_Feet_Ice_565100_1280x800.jpg)' : 'none' }}>
+    <Container style={{ height: '80vh', background: `url(${fondoImage})` }}>
       <Row className="mt-5">
         <Col xs={12} md={{ span: 6, offset: 3 }} className="text-center">
+
+          <div className='d-flex justify-content-center grid gap-2 mt-2 mb-2'>
+            <Form.Control ref={inputRef} placeholder='Ingrese su fondo'>
+            </Form.Control>
+            <Button onClick={changeImg} style={{ backgroundColor: 'pink', border: '2px solid #ff009e' }}>Cambiar</Button>
+          </div>
+
           <Button className="mb-2" variant={show ? 'danger' : 'success'} onClick={handleShowMessage} >
             {show ? 'Chauu' : 'Saluda al pingüi'}
           </Button>
