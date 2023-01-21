3bf7d8387c0b4f711597245d1c7a674ffd19bffb
meligimenez <melinagimenez.dev@gmail.com>
2023-01-21 17:47:49 -0300

    add functionality: add and substract

--- src/components/myPractices/MyCounter/index.jsx
+++ src/components/myPractices/MyCounter/index.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 export function MyCounter() {
+  const [number, setNumber] = useState(0)
+
+  const handleMore = () => {
+    setNumber(number + 1)
+  }
+
+  const handleLess = () => {
+    if (number > 0) {
+      setNumber(number - 1)
+    }
+  }
+
   return (
-    <div>MyCounter</div>
+    <Container className='mt-3' >
+      <Col className='d-flex justify-content-center'>
+        <Button onClick={handleLess} style={{ width: '50px', borderRadius: '20px' }}>-</Button>
+        <p className='mx-4 my-2 '>{number}</p>
+        <Button onClick={handleMore} style={{ width: '50px', borderRadius: '20px' }}>+</Button>
+      </Col>
+    </Container>
   )
 }
 
